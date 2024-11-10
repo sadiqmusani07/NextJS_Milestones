d89feadb533a0861fcc88c72ae463e15ee5f3733
Muhammad Sadiq Younus <sadiq.younus@maisonconsulting.com>
2024-11-10 22:26:44 +0500

    latest app updated today

--- NextJS_Milestones/milestone2_personalportfolio/my-app/src/app/page.tsx
+++ NextJS_Milestones/milestone2_personalportfolio/my-app/src/app/page.tsx
@@ -1,100 +1,127 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
+import Link from 'next/link';
+import Skills from '../components/Skills';
+import ExperienceItem from '../components/ExperienceItem';
+import ProjectCard from '../components/ProjectCard';
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const skills = [
+    { title: 'JavaScript', imageUrl: '/js.png' },
+    { title: 'TypeScript', imageUrl: '/ts.png' },
+    { title: 'C#', imageUrl: '/csharp.png' },
+    { title: 'HTML', imageUrl: '/html.png' },
+    { title: 'CSS', imageUrl: '/css.png' },
+    { title: 'NEXT.js', imageUrl: '/nextjs.png' },
+    { title: 'React.js', imageUrl: '/reactjs.png' },
+    { title: 'MS D365 CRM', imageUrl: '/d365.png' },
+  ];
+
+  const projects = [
+    { title: 'Resume Builder Project', description: 'Build and Interactive Resume Builder Project helping people to create free resumes', imageUrl: '/resume.png' },
+    { title: 'Personal Portfolio Project', description: 'A personal portfolio project listing my work and achievements', imageUrl: '/portfolio.png' },
+    { title: 'Dynamics 365 Projects', description: 'Created many d365 CRM plugins for model driven apps', imageUrl: '/modeldriven.png' },
+  ];
+
+  const experiences = [
+    {
+      company: 'Maison Consulting and Solution',
+      logoUrl: '/maison.png',
+      role: 'CRM Customization Consultant',
+      duration: 'Dec-2023 - Present',
+    },
+    {
+      company: 'Global ITS',
+      logoUrl: '/gits.png',
+      role: 'Associate Dynamcis CRM Consultant',
+      duration: 'Sep-2022 - Dec-2023',
+    },
+    {
+      company: 'Friends Bit',
+      logoUrl: '/fbits.png',
+      role: 'Web Developer',
+      duration: '5 Months',
+    },
+    {
+      company: 'Liquid Technologies',
+      logoUrl: '/liquid.png',
+      role: 'Intern React JS Developer',
+      duration: '10 Months',
+    },
+  ];
+
+  if (!isClient) {
+    return null;
+  }
+
+  return (
+    <div>
+      <header>
+        <Link href="#top" className="logo">Sadiq M Younus - Portfolio Website</Link>
+        <nav>
+          <Link href="#about">About</Link>
+          <Link href="#skills">Skills</Link>
+          <Link href="#experience">Experience</Link>
+          <Link href="#projects">Projects</Link>
+          <Link href="#future-goals">Future Goals</Link>
+        </nav>
+      </header>
+
+      <section id="about">
+        <h1>About Me</h1>
+        <p>I am Sadiq, a passionate developer having hands-on experience in Microsoft Dynamcis CRM as a technical consultant and Web Development especially in react and next. Always eager to learn the keen concepts and the working behind the technologies and apps.</p>
+      </section>
+
+      <section id="skills">
+        <div className="skills">
+          {skills.map((skill, index) => (
+            <div className="skill-item" key={index}>
+              <img src={skill.imageUrl} alt={skill.title} />
+              <h3>{skill.title}</h3>
+            </div>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      </section>
+
+      <section id="experience">
+        <h2>Experience</h2>
+        <div className="experience-slider">
+          {experiences.map((exp, index) => (
+            <ExperienceItem
+              key={index}
+              company={exp.company}
+              logoUrl={exp.logoUrl}
+              role={exp.role}
+              duration={exp.duration}
+            />
+          ))}
+        </div>
+      </section>
+
+      <section id="projects">
+        <h2>Projects</h2>
+        <div className="projects-slider">
+          {projects.map((project, index) => (
+            <ProjectCard key={index} {...project} />
+          ))}
+        </div>
+      </section>
+
+      <section id="future-goals">
+        <h2>Future Goals/Plannings</h2>
+        <p>My future goal is to learn about the advancement of AI and how it can make the real difference in the uplifting of people life.</p>
+      </section>
+
+      <footer>
+        <p>© 2024 SADIQ M YOUNUS - My Portfolio Project. All rights reserved</p>
       </footer>
     </div>
   );
